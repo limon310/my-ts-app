302373af53a9ddc9f16b20801fd2b315e2bc3f64
Md.limon islam <mdlimonislam134@gmail.com>
2026-06-24 09:46:01 +0600

    Typescript assignment

---
+++ src/index.ts
@@ -0,0 +1,117 @@
+//*******/ Primitive Data Type *******
+
+let name: string = "Limon";
+let age: number = 25;
+let isStudent: boolean = true;
+
+console.log(name);
+console.log(age);
+console.log(isStudent);
+
+// ********* Array *********
+
+let names: string[] = ["Limon", "Milon", "Ripon"]
+let numbers: number[] = [11,22,33,44]
+
+names.push("Naim")
+numbers.push(55)
+
+console.log(names)
+console.log(numbers)
+
+// ******** Object *********
+
+const user: {
+    name: string,
+    age: number,
+    isActive: boolean
+} = {
+    name: "Md Limon Islam",
+    age: 21,
+    isActive: true
+}
+
+console.log(user)
+
+// object extends using interface
+interface User {
+  id: number;
+  username: string;
+}
+
+interface AdminUser extends User {
+  role: 'admin' | 'superadmin';
+  permissions: string[];
+}
+
+// Usage
+const admin: AdminUser = {
+  id: 2585,
+  username: "alice_dev",
+  role: "admin",
+  permissions: ["manage_users", "delete_posts"]
+};
+console.log(admin)
+
+// ******* Function **********
+function add(a: number, b: number): number {
+  return a + b;
+}
+
+const result = add(10, 20);
+
+console.log(result);
+
+// generics
+
+function identity<T>(argument: T ): T {
+    return argument
+}
+
+const output = identity("limon");
+console.log(output)
+
+// ****** Read Only ******
+
+type Person = {
+  readonly id: number;  // read only change kora jabe nah
+  name: string;
+  email?: string;  // optional
+};
+
+const person: Person = {
+  id: 1,
+  name: "Limon"
+};
+
+console.log(person);
+
+
+// ********* Type Vs InterFace*******
+// Type-----------
+
+type People = {
+  name: string;
+  age: number;
+};
+
+const p1: People = {
+  name: "Limon",
+  age: 25
+};
+
+// console.log(p1)
+
+// interface
+
+interface Student {
+  name: string;
+  roll: number;
+}
+
+const s1: Student = {
+  name: "Hasan",
+  roll: 10
+};
+
+// console.log(s1);
